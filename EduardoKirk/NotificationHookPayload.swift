c80270f023dcd342bbf67cfabd3543e8567ad283
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 07:40:28 +0900

    add NotificationHookPayload

---
+++ EduardoKirk/NotificationHookPayload.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationHookPayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/16.
+//
+
+import Foundation
+
+struct NotificationHookPayload: Codable {
+    let sessionId: String
+    let transcriptPath: String
+    let cwd: String
+    let hookEventName: String
+    let message: String
+    let notificationType: String
+
+    enum CodingKeys: String, CodingKey {
+        case sessionId = "session_id"
+        case transcriptPath = "transcript_path"
+        case cwd
+        case hookEventName = "hook_event_name"
+        case message
+        case notificationType = "notification_type"
+    }
+}
