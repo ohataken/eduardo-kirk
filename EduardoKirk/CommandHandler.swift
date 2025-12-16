f0b5b890aa9dbd3a1f7602107afda034c62153d9
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-16 18:54:41 +0900

    add isSessionEnd

--- EduardoKirk/CommandHandler.swift
+++ EduardoKirk/CommandHandler.swift
@@ -9,4 +9,8 @@ struct CommandHandler {
     private static func isSessionStart(_ args: [String]) -> Bool {
         return args.count > 1 && args[1] == "session-start"
     }
+
+    private static func isSessionEnd(_ args: [String]) -> Bool {
+        return args.count > 1 && args[1] == "session-end"
+    }
 }
