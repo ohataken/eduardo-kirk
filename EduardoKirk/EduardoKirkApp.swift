949097eb475f2e435620e2bbfb02f255999228e6
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-16 15:41:39 +0900

    Initial Commit

---
+++ EduardoKirk/EduardoKirkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduardoKirkApp.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct EduardoKirkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EduardoKirkTests/EduardoKirkTests.swift
@@ -0,0 +1,17 @@
+//
+//  EduardoKirkTests.swift
+//  EduardoKirkTests
+//
+//  Created by ohataken on 2025/12/16.
+//
+
+import Testing
+@testable import EduardoKirk
+
+struct EduardoKirkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
