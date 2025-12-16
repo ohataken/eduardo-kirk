e4c488d675664f5b26f6ae9ad850df7dbfddb8ef
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 07:40:15 +0900

    add SessionStartHookPayload

---
+++ EduardoKirk/SessionStartHookPayload.swift
@@ -0,0 +1,24 @@
+//
+//  SessionStartHookPayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/16.
+//
+
+import Foundation
+
+struct SessionStartHookPayload: Codable {
+    let sessionId: String
+    let transcriptPath: String
+    let cwd: String
+    let hookEventName: String
+    let source: String
+
+    enum CodingKeys: String, CodingKey {
+        case sessionId = "session_id"
+        case transcriptPath = "transcript_path"
+        case cwd
+        case hookEventName = "hook_event_name"
+        case source
+    }
+}
