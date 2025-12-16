2c86189aeea6484b18b1d23527acccc6c8dcccc8
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-16 23:24:56 +0900

    update handleNotification

--- EduardoKirk/CommandHandler.swift
+++ EduardoKirk/CommandHandler.swift
@@ -79,6 +79,28 @@ struct CommandHandler {
     }
 
     private static func handleNotification(args: [String], stdin: String) async {
+        let granted = await withCheckedContinuation { continuation in
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, _ in
+                continuation.resume(returning: granted)
+            }
+        }
+
+        if granted {
+            let content = UNMutableNotificationContent()
+            content.title = "notification"
+
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+            await withCheckedContinuation { continuation in
+                UNUserNotificationCenter.current().add(request) { error in
+                    if let error = error {
+                        print("Error scheduling notification: \(error)")
+                    }
+                    continuation.resume()
+                }
+            }
+        }
     }
 
     private static func handleStop(args: [String], stdin: String) async {
