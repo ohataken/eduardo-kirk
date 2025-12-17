c65669a885fa647829da3bb22bae89ae229ddfe5
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 23:28:56 +0900

    copy NotificationHookPayload

---
+++ EduardoKirkCommand/NotificationHookPayload.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationHookPayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/17.
+//
+
+struct NotificationHookPayload: Codable {
+    let sessionId: String
+    let transcriptPath: String
+    let cwd: String
+    let hookEventName: String
+
+    enum CodingKeys: String, CodingKey {
+        case sessionId = "session_id"
+        case transcriptPath = "transcript_path"
+        case cwd
+        case hookEventName = "hook_event_name"
+    }
+}
