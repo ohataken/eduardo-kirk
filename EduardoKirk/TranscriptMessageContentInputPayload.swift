80175af777f359d831f84aead111d350fe30cde9
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 18:49:44 +0900

    add TranscriptMessageContentInputPayload

---
+++ EduardoKirk/TranscriptMessageContentInputPayload.swift
@@ -0,0 +1,11 @@
+//
+//  TranscriptMessageContentInputPayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/17.
+//
+
+struct TranscriptMessageContentInputPayload: Codable {
+    let description: String?
+    let plan: String?
+}
