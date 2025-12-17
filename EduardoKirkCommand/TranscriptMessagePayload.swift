f641028832c50cf50bd5ec2b9325ea8d62840b24
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 23:32:13 +0900

    copy TranscriptMessagePayload

---
+++ EduardoKirkCommand/TranscriptMessagePayload.swift
@@ -0,0 +1,26 @@
+//
+//  TranscriptMessagePayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/17.
+//
+
+struct TranscriptMessagePayload: Codable {
+    let model: String
+    let id: String
+    let type: String
+    let role: String
+    let content: [TranscriptMessageContentPayload]
+    let stopReason: String?
+    let stopSequence: String?
+
+    enum CodingKeys: String, CodingKey {
+        case model
+        case id
+        case type
+        case role
+        case content
+        case stopReason = "stop_reason"
+        case stopSequence = "stop_sequence"
+    }
+}
