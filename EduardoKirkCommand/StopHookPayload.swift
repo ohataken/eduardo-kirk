1f6fc79d1a964aa8558506ca86204db72eb56e25
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-17 23:29:06 +0900

    copy StopHookPayload

---
+++ EduardoKirkCommand/StopHookPayload.swift
@@ -0,0 +1,24 @@
+//
+//  StopHookPayload.swift
+//  EduardoKirk
+//
+//  Created by ohataken on 2025/12/17.
+//
+
+struct StopHookPayload: Codable {
+    let sessionId: String
+    let transcriptPath: String
+    let cwd: String
+    let permissionMode: String
+    let hookEventName: String
+    let stopHookActive: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case sessionId = "session_id"
+        case transcriptPath = "transcript_path"
+        case cwd
+        case permissionMode = "permission_mode"
+        case hookEventName = "hook_event_name"
+        case stopHookActive = "stop_hook_active"
+    }
+}
