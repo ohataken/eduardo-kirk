f245d3467e22732361f6fc2f7db2545a5d56908a
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-20 23:58:33 +0900

    chore: release homebrew 0.1.0 (#36)

--- Formula/eduardo-kirk.rb
+++ Formula/eduardo-kirk.rb
@@ -1,9 +1,9 @@
 class EduardoKirk < Formula
   desc "desktop notifications for Claude Code"
   homepage "https://github.com/ohataken/eduardo-kirk"
-  version "0.0.0"
+  version "0.1.0"
   url "https://github.com/ohataken/eduardo-kirk/releases/download/v#{version}/eduardo-kirk-#{version}.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "7401f20e9ff967bdff47c568089b5ce97e788d5fc96a980a2eb790163a563e65"
   
   def install
     bin.install "eduardo-kirk"
