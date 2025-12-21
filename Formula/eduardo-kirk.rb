436293e000135d4f62604b787d779424afa7c051
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-21 18:32:22 +0900

    chore: bump version to 0.1.1 (#42)

--- Formula/eduardo-kirk.rb
+++ Formula/eduardo-kirk.rb
@@ -1,7 +1,7 @@
 class EduardoKirk < Formula
   desc "desktop notifications for Claude Code"
   homepage "https://github.com/ohataken/eduardo-kirk"
-  version "0.1.0"
+  version "0.1.1"
   url "https://github.com/ohataken/eduardo-kirk/releases/download/v#{version}/eduardo-kirk-#{version}.tar.gz"
   sha256 "7401f20e9ff967bdff47c568089b5ce97e788d5fc96a980a2eb790163a563e65"
   
