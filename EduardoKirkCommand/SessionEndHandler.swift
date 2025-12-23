8c068c6a78bd4a9caf2b951345eb74e345b67bd8
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-23 10:48:42 +0900

    chore: 通知のタイトルの文言を整えた

--- EduardoKirkCommand/SessionEndHandler.swift
+++ EduardoKirkCommand/SessionEndHandler.swift
@@ -26,7 +26,7 @@ struct SessionEndHandler: CommandHandlerProtocol {
 
         let process = Process()
         process.executableURL = URL(fileURLWithPath: "/usr/bin/osascript")
-        process.arguments = ["-e", "display notification \"handleSessionEnd\" with title \"handleSessionEnd\" sound name \"Glass\""]
+        process.arguments = ["-e", "display notification \"handleSessionEnd\" with title \"SessionEnd - Claude Code\" sound name \"Glass\""]
         try? process.run()
         process.waitUntilExit()
     }
