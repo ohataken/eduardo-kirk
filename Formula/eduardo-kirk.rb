0952b88413b93036a80f838e333a87800676e52c
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-26 11:57:06 +0900

    squash

--- Formula/eduardo-kirk.rb
+++ Formula/eduardo-kirk.rb
@@ -1,7 +1,7 @@
 class EduardoKirk < Formula
   desc "desktop notifications for Claude Code"
   homepage "https://github.com/ohataken/eduardo-kirk"
-  version "0.1.4"
+  version "0.1.5"
   url "https://github.com/ohataken/eduardo-kirk/releases/download/v#{version}/eduardo-kirk-#{version}.tar.gz"
   sha256 "4095929e2fc617599c9a3df8c14f90842a5c5f2a220ccc811fd51398ce2e4262"
   
