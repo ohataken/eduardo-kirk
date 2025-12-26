8a25dbb19e9027e63bae91eac36ed87984eb0038
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-26 11:04:37 +0900

    test: session-end

--- spec/session_end/session_end_spec.rb
+++ spec/session_end/session_end_spec.rb
@@ -1,4 +1,19 @@
 require_relative "../spec_helper"
 
 RSpec.describe "EduardoKirk session-end" do
+  context "" do
+    it "outputs nothing to stdout and stderr" do
+      stdin_data = {
+        session_id: "00000000-0000-0000-0000-000000000000",
+        transcript_path: Pathname.new(__dir__).to_s,
+        cwd: Pathname.getwd.to_s,
+        hook_event_name: "SessionEnd",
+        reason: "prompt_input_exit"
+      }
+
+      stdout, stderr, status = Open3.capture3(ENV["EDUARDO_KIRK_PATH"], "session-end", stdin_data: stdin_data.to_json)
+      expect(stdout).to be_empty
+      expect(stderr).to be_empty
+    end
+  end
 end
