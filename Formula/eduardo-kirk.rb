2c68f076c34a51a9fcebfa05b4eac041ce6b4b2f
ohataken <11092425+ohataken@users.noreply.github.com>
2026-01-24 08:01:47 +0900

    chore: update SHA (#82)

--- Formula/eduardo-kirk.rb
+++ Formula/eduardo-kirk.rb
@@ -3,7 +3,7 @@ class EduardoKirk < Formula
   homepage "https://github.com/ohataken/eduardo-kirk"
   version "0.1.9"
   url "https://github.com/ohataken/eduardo-kirk/releases/download/v#{version}/eduardo-kirk-#{version}.tar.gz"
-  sha256 "9078a5094fe7bca32c1ed2875ebe916f56e802293c0c359a34e77e816ce99ee8"
+  sha256 "aa90cc7533b43d607cdf24b3096f614ad66723bd5bc4c70803eb939f8070f26f"
   
   def install
     bin.install "EduardoKirkCommand" => "eduardo-kirk"
