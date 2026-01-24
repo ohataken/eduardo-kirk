86ad115ac8058bdfcf505cf007fbb19bfd84ea39
ohataken <11092425+ohataken@users.noreply.github.com>
2026-01-24 18:23:12 +0900

    refactor: TranscriptPayload の timestamp を Date? 型にした (#99)

--- EduardoKirkCommand/TranscriptPayload.swift
+++ EduardoKirkCommand/TranscriptPayload.swift
@@ -5,12 +5,14 @@
 //  Created by ohataken on 2025/12/17.
 //
 
+import Foundation
+
 struct TranscriptPayload: Codable {
     let type: String
     let uuid: String?
     let sessionId: String?
     let userType: String?
     let cwd: String?
-    let timestamp: String?
+    let timestamp: Date?
     let message: TranscriptMessagePayload?
 }
