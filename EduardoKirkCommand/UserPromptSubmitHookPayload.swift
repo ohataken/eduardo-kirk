b9f17e95a7e5109bb78e72195fb8e8a62ac435d4
ohataken <11092425+ohataken@users.noreply.github.com>
2026-01-25 17:28:21 +0900

    feat: UserPromptSubmit title に cwd の lastPathComponent を表示する (#112)

--- EduardoKirkCommand/UserPromptSubmitHookPayload.swift
+++ EduardoKirkCommand/UserPromptSubmitHookPayload.swift
@@ -5,6 +5,8 @@
 //  Created by ohataken on 2025/12/17.
 //
 
+import Foundation
+
 struct UserPromptSubmitHookPayload: Codable {
     let sessionId: String
     let transcriptPath: String
@@ -18,5 +20,9 @@ struct UserPromptSubmitHookPayload: Codable {
         case cwd
         case hookEventName = "hook_event_name"
         case prompt
+    }
+
+    var cwdURL: URL {
+        URL(fileURLWithPath: cwd)
     }
 }
