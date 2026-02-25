2315de42ea2f7db3e07090708ac55881e9b3a82f
ohataken <11092425+ohataken@users.noreply.github.com>
2026-02-25 23:45:57 +0900

    chore: エラーメッセージを改善する (#133)

--- EduardoKirkCommand/UserPromptSubmitHandler.swift
+++ EduardoKirkCommand/UserPromptSubmitHandler.swift
@@ -16,7 +16,7 @@ struct UserPromptSubmitHandler: CommandHandlerProtocol {
     
     static func handle(args: [String], stdin: String) {
         guard !stdin.isEmpty, let data = stdin.data(using: .utf8) else {
-            print("Failed to decode UserPromptSubmitHookPayload")
+            print("No input received from stdin")
             return
         }
 
