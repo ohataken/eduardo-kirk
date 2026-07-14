7630616d2ad827f7365657988d93a3300d5f84ee
ohataken <11092425+ohataken@users.noreply.github.com>
2026-07-14 16:13:47 +0900

    chore: update Formula for 0.1.19

--- Formula/eduardo-kirk.rb
+++ Formula/eduardo-kirk.rb
@@ -1,9 +1,9 @@
 class EduardoKirk < Formula
   desc "desktop notifications for Claude Code"
   homepage "https://github.com/ohataken/eduardo-kirk"
-  version "0.1.18"
+  version "0.1.19"
   url "https://github.com/ohataken/eduardo-kirk/releases/download/v#{version}/eduardo-kirk-#{version}.tar.gz"
-  sha256 "92c106fe96682ebe7e660020ef40a57a7744fded9ce5d5d99872d4ecb484298d"
+  sha256 "4cde68a1ef964e1495e449fa9fff55bf540c95fe232efe0d4e694972127721df"
   
   def install
     bin.install "EduardoKirkCommand" => "eduardo-kirk"
